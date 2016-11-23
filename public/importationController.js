a038352b61b72aab68cc2569b23cdd5c2e8345ee
Lucas SAUVAGE <sl200103@etu.unice.fr>
2016-11-23 09:05:59 +0100

    Importation de l'atelier

--- public/importationController.js
+++ public/importationController.js
@@ -4,54 +4,22 @@
 
 app.controller('importationCtrl', function ($scope,$http) {
     $scope.importer = function () {
-       /* var fichier = document.getElementById('InputPhoto').files[0];
-        $http.get("fichier").then(function (response) {
-            console.log($scope.myData);
-            $scope.myData = response.data.records;
-        });
-    }});
 
-*/
-
-
-        var fichier = document.getElementById('InputPhoto').files[0];
+        var fichier = document.getElementById('InputJSON').files[0];
         var lecture = new FileReader();
         lecture.onloadend = function (evenement) {
             var donnees = evenement.target.result;
-            console.log("ok");
-            console.log(donnees);
 
-            var config = {
-                headers : {
-                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
-                }
-            }
-
-
-            //TODO post
-
-            /* $http.post('/', donnees, config)
-                .success(function (donnees, status, headers, config) {
-                    $scope.PostDataResponse = donnees;
-                    console.log("cest fait");
-                })
-                .error(function (donnes, status, header, config) {
-                    $scope.ResponseDetails = "Donnees: " + donnees +
-                        "<hr />status: " + status +
-                        "<hr />headers: " + header +
-                        "<hr />config: " + config;
-                });
-
-*/
-
-//traitement
-
-
+            var res = $http.post('/api/v1/catalogue', donnees);
+            res.success(function(data, status, headers, config) {
+                $scope.message = data;
+                alert("Succes");
+            });
+            res.error(function(data, status, headers, config) {
+                alert( "failure message: " + JSON.stringify({data: data}));
+            });
         }
-        lecture.readAsBinaryString(fichier)
-
-       // console.log(lecture.readAsBinaryString(fichier));
-       // console.log(a);
+        lecture.readAsText(fichier, 'UTF-8')
     }
 });
 
