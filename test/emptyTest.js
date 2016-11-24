590f7e738cc645c9f190525f86f9356da835cf32
PierreMassanes <pierre.massanes@gmail.com>
2016-11-24 10:13:32 +0100

    Updated useless test

--- test/emptyTest.js
+++ test/emptyTest.js
@@ -8,7 +8,7 @@ var expect = chai.expect;
 describe('This is an empty test suite', function(){
     describe('This is an empty section test',function(){
         it('should be ok because it is empty',function(){
-           expect(true).to.be.ok;
+           expect(true).to.be.true;
         });
     });
 });
