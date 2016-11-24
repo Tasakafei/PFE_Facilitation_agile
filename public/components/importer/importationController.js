87fb17ed3e34ccbebfe9a57afea09c123208b63b
Lucas SAUVAGE <sl200103@etu.unice.fr>
2016-11-24 10:20:18 +0100

    Vérification du format JSON et de certain éléments clefs avant importation

--- public/components/importer/importationController.js
+++ public/components/importer/importationController.js
@@ -10,21 +10,44 @@ app.controller('importationCtrl', function ($scope,$http) {
         lecture.onloadend = function (evenement) {
             var donnees = evenement.target.result;
 
-            var res = $http.post('/api/v1/catalogue', donnees);
-            res.success(function(data, status, headers, config) {
-                $scope.message = data;
+            //Check if the file is a JSON
+            if(fichier.type == "application/json") {
 
-                $scope.$emit('notify', {
-                    type: 'success',
-                    title: 'L\'atelier a bien été importé.',
-                });
-            });
-            res.error(function(data, status, headers, config) {
-                $scope.$emit('notify', {
-                    type: 'error',
-                    title: 'L\'atelier n\'a pas pu être importé.',
-                });
-            });
+                var data = JSON.parse(donnees);
+
+                //Check if the JSON file got what we expect in
+                if( data.title && data.time_max && data.synopsis && data.content.folklore && data.content.educational_aims && data.content.steps) {
+
+                    //Post
+                    var res = $http.post('/api/v1/catalogue', donnees);
+                    res.success(function(data, status, headers, config) {
+                        $scope.message = data;
+
+                        $scope.$emit('notify', {
+                            type: 'success',
+                            title: 'L\'atelier a bien été importé.',
+                        });
+                    });
+                    res.error(function(data, status, headers, config) {
+                        $scope.$emit('notify', {
+                            type: 'error',
+                            title: 'L\'atelier n\'a pas pu être importé.',
+                        });
+                    });
+                } else {
+                    $scope.$emit('notify', {
+                        type: 'error',
+                        title: 'Format de l\'atelier incomptatible.',
+                    });
+                }
+            }
+            else {
+                    $scope.$emit('notify', {
+                        type: 'error',
+                        title: 'L\'atelier doit être en format JSON.',
+                    });
+            }
+
         }
         lecture.readAsText(fichier, 'UTF-8')
     }
