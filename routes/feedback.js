d0b24f70e1b7a585b68135b9a86e370228ae3560
Alexandre CAZALA <alexandre.cazala@gmail.com>
2016-12-01 11:40:30 +0100

    [FIX][SERVER] Upload de photo

--- routes/feedback.js
+++ routes/feedback.js
@@ -9,11 +9,20 @@ var router = express.Router();
 var auth = require('../configurations/auth');
 var instances = require('../controllers/instances');
 var multer  = require('multer');
-var upload = multer({ dest: '../uploads/' });
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname+ '-' + Date.now()+'.jpg')
+    }
+});
+
+var upload = multer({ storage: storage });
 
 
 /*** User routes ***/
 router.post('/:instanceId', instances.addFeedbackToInstance);
-router.post('/:instanceId/picture', upload.single('photo.jpg'), instances.UploadPhoto);
+router.post('/:instanceId/picture', upload.single('file'), instances.UploadPhoto);
 router.post('/:instanceId/picture', instances.UploadPhoto);
 module.exports = router;
