5a6bca56175db1a604c9b6a245dac3006c8fe3ef
Alexandre CAZALA <alexandre.cazala@gmail.com>
2017-01-22 22:00:13 +0100

    [DEV#52] Bug fixed (the format changed so it could not access the right properties)

--- public/components/home/home.controller.js
+++ public/components/home/home.controller.js
@@ -13,12 +13,13 @@ app.controller('homeCtrl', function ($scope, CatalogueDataProvider) {
      * Put in the $scope.dataNote workshops with a note to display them on the first page
      */
     CatalogueDataProvider.getWorkshops(function (dataResponse) {
-        $scope.data = dataResponse;
+        $scope.data = dataResponse.data;
 
         //workshop with a note
+        console.log(dataResponse);
         for(var i = 0; i < $scope.data.length; i++) {
-            if($scope.data[i].votes.participants != 0 || $scope.data[i].votes.facilitators != 0) {
-                $scope.data[i].votes.participants = ($scope.data[i].votes.participants + $scope.data[i].votes.facilitators) /2;
+            if($scope.data[i].grade.participants != 0 || $scope.data[i].grade.facilitators != 0) {
+                $scope.data[i].grade.participants = ($scope.data[i].grade.participants + $scope.data[i].grade.facilitators) /2;
                 $scope.dataNote.push($scope.data[i]);
             }
         }
