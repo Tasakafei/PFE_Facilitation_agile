e6f7eedd608c492783e6d925e179f012110fc178
PierreMassanes <pierre.massanes@gmail.com>
2017-02-27 15:52:52 +0100

    Add database test environment

--- test/emptyTest.js
+++ test/emptyTest.js
@@ -5,10 +5,15 @@
 var chai = require('chai');
 var expect = chai.expect;
 
+var config = require('../configurations/database');
+
 describe('This is an empty test suite', function(){
     describe('This is an empty section test',function(){
         it('should be ok because it is empty',function(){
-           expect(true).to.be.true;
+            console.log(config.db);
+            if(process.env.NODE_ENV === "test") console.log("TEST ENV DEFINED");
+            else console.log("PRODUCTION ENV DEFINED");
+            expect(true).to.be.true;
         });
     });
 });
